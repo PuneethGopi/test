8a96e7370414f35a193d2f95ea3047d3d6fd681e
PuneethGopi <43577019+PuneethGopi@users.noreply.github.com>
2020-06-01 15:21:43 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import WaetherCard from './WaetherCard';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <WaetherCard/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/WaetherCard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const WaetherCard = () => {
+    return ( 
+        <div className='card'> 
+            <h1>Sydney</h1>
+            <h3>Au</h3>
+            <img src="" alt="Weather icon"/>
+            <h1>20 °C</h1>
+            <h2>Clouds</h2>
+        </div>
+     );
+}
+ 
+export default WaetherCard;
